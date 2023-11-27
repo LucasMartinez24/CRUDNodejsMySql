bfdc178130690e740a98049cb9bc21781480565c
LucasMartinez24 <martinezlucas1478@gmail.com>
2023-11-27 09:45:49 -0300

    cambios

--- src/app.js
+++ src/app.js
@@ -22,7 +22,7 @@ app.set('view engine', 'hbs');
 app.use(myconnection(mysql, {
   host: 'localhost',
   user: 'root',
-  password: '',
+  password: 'Minashandball24',
   port: 3306,
   database: 'crudnodejs'
 }, 'single'));
